fb6a43b03e3badff00f9d8a30aaac3bd0dd2c534
Matt Raible <matt.raible@okta.com>
2022-03-29 16:23:03 -0600

    Updates after QA

--- src/main/java/com/regex/example/TestRegularExpressions.java
+++ src/main/java/com/regex/example/TestRegularExpressions.java
@@ -6,8 +6,8 @@ public class TestRegularExpressions {
 
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("Java", Pattern.CASE_INSENSITIVE);
-        System.out.println(pattern.matcher("java").matches());//prints true
-        System.out.println(pattern.matcher("JAVA").matches());//prints true
-        System.out.println(pattern.matcher("javascript").matches());//prints false
+        System.out.println(pattern.matcher("java").matches()); // prints true
+        System.out.println(pattern.matcher("JAVA").matches()); // prints true
+        System.out.println(pattern.matcher("javascript").matches()); // prints false
     }
 }
